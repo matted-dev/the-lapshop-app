80087a2056eb8f3517d4ef23cc28acf1712f6634
Matt Edwards <info@matted.dev>
2019-11-23 22:08:52 +0000

    Added new template, actions and route

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+  
+  def new
+    @product = Product.new
+  end
+
+  def create
+    render plain: params[:product].inspect
+    @product = Product.new(product_params)
+    @product.save
+
+    redirect_to products_show(@product)
+  end
+
+
+  private 
+    def product_params
+      params.require(:product).permit(:title, :size, :processor, :ram, :storage, :battery, :price, :description)
+    end
+end
